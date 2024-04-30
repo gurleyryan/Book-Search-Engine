ed9de23387d34d9dad632e7759eb513bc8cc0125
Ryan Gurley <gurleyryan@gmail.com>
2024-04-30 11:14:21 -0400

    updated dependencies and auth.js middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,20 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = process.env.AUTH_SECRET;
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
